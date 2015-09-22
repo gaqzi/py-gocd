ea0e696156432e21b2e7e20c4ddab5b43630971a
Björn Andersson <bjorn@neo.com>
2015-09-22 09:33:30 +0800

    Add automatic retry to Artifact.get_directory
    Since Go returns a 202 by default it shouldn't be up to the standard
    user to have to keep track of having to retry.
    
    The values used for backoff and so on are pretty random and have
    worked well enough for me in testing. Hoping it'll be good enough for
    most use cases.

--- gocd/api/artifact.py
+++ gocd/api/artifact.py
@@ -1,4 +1,5 @@
 from gocd.api.endpoint import Endpoint
+import time
 
 
 class Artifact(Endpoint):
@@ -61,18 +62,58 @@ class Artifact(Endpoint):
         """
         return self._get(path_to_file)
 
-    def get_directory(self, path_to_directory):
+    def get_directory(self, path_to_directory, timeout=30, backoff=0.4, max_wait=4):
         """Gets an artifact directory by its path.
 
         See the `Go artifact directory documentation`__ for example responses.
 
         .. __: http://api.go.cd/current/#get-artifact-directory
 
+        .. note::
+          Getting a directory relies on Go creating a zip file of the
+          directory in question. Because of this Go will zip the file in
+          the background and return a 202 Accepted response. It's then up
+          to the client to check again later and get the final file.
+
+          To work with normal assumptions this :meth:`get_directory` will
+          retry itself up to ``timeout`` seconds to get a 200 response to
+          return. At that point it will then return the response as is, no
+          matter whether it's still 202 or 200. The retry is done with an
+          exponential backoff with a max value between retries. See the
+          ``backoff`` and ``max_wait`` variables.
+
+          If you want to handle the retry logic yourself then use :meth:`get`
+          and add '.zip' as a suffix on the directory.
+
         Args:
           path_to_directory (str): The path to the directory to get.
-            It can be nested eg ```target/dist.zip```
+            It can be nested eg ``target/dist.zip``
+          timeout (int): How many seconds we will wait in total for a
+            successful response from Go when we're receiving 202
+          backoff (float): The initial value used for backoff, raises
+            exponentially until it reaches ``max_wait``
+          max_wait (int): The max time between retries
 
         Returns:
           Response: :class:`gocd.api.response.Response` object
+            A successful response is a zip-file.
         """
-        return self._get('{0}.zip'.format(path_to_directory))
+        response = None
+        started_at = None
+        time_elapsed = 0
+
+        i = 0
+        while time_elapsed < timeout:
+            response = self._get('{0}.zip'.format(path_to_directory))
+
+            if response:
+                break
+            else:
+                if started_at is None:
+                    started_at = time.time()
+
+                time.sleep(min(backoff * (2 ** i), max_wait))
+                i += 1
+                time_elapsed = time.time() - started_at
+
+        return response
