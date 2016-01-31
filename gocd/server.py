fe0d711f9c6f4dccd1a0f6ff3b9a4045e938dd16
Björn Andersson <ba@sanitarium.se>
2016-01-31 21:49:49 +0800

    Add documentation for Server.stage

--- gocd/server.py
+++ gocd/server.py
@@ -206,6 +206,16 @@ class Server(object):
         return PipelineGroups(self)
 
     def stage(self, pipeline_name, stage_name, pipeline_counter=None):
+        """Returns an instance of :class:`Stage`
+
+        Args:
+            pipeline_name (str): Name of the pipeline the stage belongs to
+            stage_name (str): Name of the stage to act on
+            pipeline_counter (int): The pipeline instance the stage is for.
+
+        Returns:
+          Stage: an instantiated :class:`Stage`.
+        """
         return Stage(self, pipeline_name, stage_name, pipeline_counter=pipeline_counter)
 
     def _add_basic_auth(self):
