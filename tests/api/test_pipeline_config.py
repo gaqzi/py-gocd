beaf932c50776a1a4d82003e185e25a680c90013
Henrique Gemignani Passos Lima <henrique.gemignani@tapps.com.br>
2016-05-03 17:53:01 -0300

    Add PipelineConfig.get failure test.

--- tests/api/test_pipeline_config.py
+++ tests/api/test_pipeline_config.py
@@ -16,7 +16,17 @@ def test_get_existing(server):
 
     response = api_config.get()
 
+    assert response.is_ok
     assert response.etag is not None
     assert response["name"] == "PyGoCd"
     assert response["template"] is None
     assert response["stages"][0]["name"] == "defaultStage"
+
+
+@vcr.use_cassette('tests/fixtures/cassettes/api/pipeline-config/get-missing.yml')
+def test_get_missing(server):
+    api_config = gocd.api.PipelineConfig(server, "MissingPipeline")
+
+    response = api_config.get()
+
+    assert not response.is_ok
