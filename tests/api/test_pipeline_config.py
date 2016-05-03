feab160ccf7adb088043d9f0849271dc38a95847
Henrique Gemignani Passos Lima <henrique.gemignani@tapps.com.br>
2016-05-03 17:45:22 -0300

    Add PipelineConfig.get successful test.

---
+++ tests/api/test_pipeline_config.py
@@ -0,0 +1,22 @@
+import pytest
+import vcr
+
+import gocd
+import gocd.api
+
+
+@pytest.fixture
+def server():
+    return gocd.Server('http://192.168.99.100:8153', user='bot', password='12345678')
+
+
+@vcr.use_cassette('tests/fixtures/cassettes/api/pipeline-config/get-successful.yml')
+def test_get_existing(server):
+    api_config = gocd.api.PipelineConfig(server, "PyGoCd")
+
+    response = api_config.get()
+
+    assert response.etag is not None
+    assert response["name"] == "PyGoCd"
+    assert response["template"] is None
+    assert response["stages"][0]["name"] == "defaultStage"
